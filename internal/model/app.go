03bf5b4072445d30ee4922db391fe7131be7648b
mohsen <mohsen.salehi2070@gmail.com>
2025-10-20 11:10:42 +0330

    Update app.go

--- internal/model/app.go
+++ internal/model/app.go
@@ -1,12 +1,26 @@
 package model
 
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/theme"
+)
+
 type App struct {
+	App        fyne.App
 	Name       string
 	MainWindow MainWindow
 }
 
 func NewApp(name string) *App {
+
+	myApp := app.New()
+
+	iconResource := theme.FyneLogo()
+	myApp.SetIcon(iconResource)
+
 	return &App{
 		Name: name,
+		App:  myApp,
 	}
 }
