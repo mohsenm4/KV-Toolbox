fa75614d890bd908adab5c5bc6dd3afd7fa60e1b
mohsen <mohsen.salehi2070@gmail.com>
2025-10-25 08:21:59 +0330

    delete formpaste in share

--- internal/filterdatabase/badger/badger.go
+++ internal/filterdatabase/badger/badger.go
@@ -2,15 +2,12 @@ package Filterbadger
 
 import (
 	"DatabaseDB/internal/filterdatabase"
-	sharedfunc "DatabaseDB/internal/filterdatabase/SharedFunc"
 	"io/ioutil"
 	"log"
 	"path/filepath"
 
-	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/storage"
-	"fyne.io/fyne/v2/widget"
 )
 
 type NameDatabaseBadger struct{}
@@ -40,9 +37,4 @@ func (l *NameDatabaseBadger) FilterFile(path string) bool {
 
 func (l *NameDatabaseBadger) FilterFormat(folderDialog *dialog.FileDialog) {
 	folderDialog.SetFilter(storage.NewExtensionFileFilter([]string{".sst", ".vlog"}))
-}
-
-func (l *NameDatabaseBadger) FormCreate(a fyne.App, title string, lastColumnContent *fyne.Container, rightColumnContentORG *fyne.Container, nameButtonProject *widget.Label, buttonAdd *widget.Button, buttonSearch *widget.Button, buttonDelete *widget.Button, columnEditKey *fyne.Container, saveKey *widget.Button, mainWindow fyne.Window) {
-	sharedfunc.FormPasteDatabase(a, title, lastColumnContent, rightColumnContentORG, nameButtonProject, buttonAdd, buttonSearch, buttonDelete, columnEditKey, saveKey, mainWindow)
-
 }
