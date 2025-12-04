3ca05244bb6997afe347730a21abbbc504fd4221
mohsen <mohsen.salehi2070@gmail.com>
2025-12-04 14:33:13 +0330

    Update main.go

--- cmd/KV-Toolbox/main.go
+++ cmd/KV-Toolbox/main.go
@@ -3,17 +3,12 @@ package main
 import (
 	"DatabaseDB/internal/pref"
 	"DatabaseDB/internal/ui/mainwindow"
-	"net/http"
 	_ "net/http/pprof"
 
 	"fyne.io/fyne/v2/app"
 )
 
 func main() {
-
-	go func() {
-		http.ListenAndServe("localhost:6060", nil) // pprof server
-	}()
 	myApp := app.NewWithID("com.DatabaseDB.KV-Toolbox")
 
 	window := mainwindow.NewMainWindow("ManageDB")
