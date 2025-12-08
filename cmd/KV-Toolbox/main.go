46c1a24421f29c09ea7459c5479c4a79f7fdf5b1
mohsen <mohsen.salehi2070@gmail.com>
2025-12-08 11:01:11 +0330

    Update main.go

--- cmd/KV-Toolbox/main.go
+++ cmd/KV-Toolbox/main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"DatabaseDB/internal/pref"
 	"DatabaseDB/internal/ui/mainwindow"
-	"os"
 
 	"fyne.io/fyne/v2/app"
-	"github.com/syndtr/goleveldb/leveldb"
 )
 
 func main() {
@@ -19,20 +17,4 @@ func main() {
 
 	window.MainWindow(myApp)
 
-	select {}
-
-	db, err := leveldb.OpenFile("/Users/macbookpro/Desktop/le", nil)
-	if err != nil {
-		panic(err)
-	}
-
-	defer db.Close()
-	// value is file in this /Users/macbookpro/Desktop/
-
-	file, err := os.ReadFile("/Users/macbookpro/Desktop/mm.mp3")
-	if err != nil {
-		panic(err)
-	}
-	db.Put([]byte("key"), file, nil)
-	return
 }
